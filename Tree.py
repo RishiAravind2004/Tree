4caa98341d5bbec23ff1337b35bc6eebd8b5a7ed
Mr.Potter <95698888+RishiAravind2004@users.noreply.github.com>
2022-12-03 19:22:04 +0530

    Create Tree.py

---
+++ Tree.py
@@ -0,0 +1,27 @@
+import turtle as t
+t.speed(0)
+t.pensize(2)
+t.left(90)
+t.backward(100)
+t.color("green")
+
+#RECURSION
+def draw(l):
+	if(l<10):
+		return
+	else:
+		t.forward(l)
+		t.color("red")
+		t.circle(2)
+		t.color("green")
+		t.left(30)
+		draw(3*l/4)
+		t.right(60)
+		draw(3*l/4)
+		t.left(30)
+		t.backward(l)
+
+draw(100)
+t.exitonclick()
+
+#By RishiAravind2004
